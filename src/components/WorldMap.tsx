ee48697decedadb4b0e44deed70bc83b52e36fc1
Jan Potoms <2109932+Janpot@users.noreply.github.com>
2021-01-09 09:23:32 +0100

    Improve map code

--- src/components/WorldMap.tsx
+++ src/components/WorldMap.tsx
@@ -3,8 +3,8 @@ import * as topojson from 'topojson-client';
 import * as React from 'react';
 import { Mercator } from '@visx/geo';
 import { ParentSize } from '@visx/responsive';
-import { scaleLinear, PickD3Scale } from '@visx/scale';
-import { Tooltip, useTheme } from '@material-ui/core';
+import { scaleLinear } from '@visx/scale';
+import { lighten, Tooltip, useTheme, TooltipProps } from '@material-ui/core';
 
 interface FeatureShape {
   type: 'Feature';
@@ -24,64 +24,73 @@ const numberFormatPercent = new Intl.NumberFormat('en', {
   maximumFractionDigits: 2,
 });
 
-interface CountryProps {
-  path: string;
-  color: PickD3Scale<'linear', string>;
-  value?: number;
-  name: string;
+interface ExtendedTooltipProps extends TooltipProps {
+  // will be part of matui v5:
+  followCursor?: boolean;
 }
 
-function Country({ path, color, value, name }: CountryProps) {
+const ExtendedTooltip = React.forwardRef<typeof Tooltip, ExtendedTooltipProps>(
+  ({ followCursor, ...props }: ExtendedTooltipProps) => {
+    const positionRef = React.useRef({ x: 0, y: 0 });
+    const popperRef = React.useRef<any>(null);
+
+    const handleMouseMove = (event: React.MouseEvent) => {
+      positionRef.current = { x: event.clientX, y: event.clientY };
+
+      if (popperRef.current) {
+        popperRef.current.scheduleUpdate();
+      }
+    };
+
+    const extendedTooltipProps = followCursor
+      ? {
+          onMouseMove: handleMouseMove,
+          PopperProps: {
+            popperRef,
+            anchorEl: {
+              clientHeight: 0,
+              clientWidth: 0,
+              getBoundingClientRect: () => ({
+                top: positionRef.current.y,
+                left: positionRef.current.x,
+                right: positionRef.current.x,
+                bottom: positionRef.current.y,
+                width: 0,
+                height: 0,
+              }),
+            },
+          },
+        }
+      : {};
+
+    return <Tooltip {...props} {...extendedTooltipProps} />;
+  }
+);
+
+interface CountryProps {
+  path: string;
+  color: string;
+  tooltip: React.ReactElement | string;
+}
+
+function Country({ path, color, tooltip }: CountryProps) {
   const theme = useTheme();
 
   const [hover, setHover] = React.useState(false);
-
-  const positionRef = React.useRef({ x: 0, y: 0 });
-  const popperRef = React.useRef<any>(null);
-
-  const handleMouseMove = (event: React.MouseEvent) => {
-    positionRef.current = { x: event.clientX, y: event.clientY };
-
-    if (popperRef.current) {
-      popperRef.current.scheduleUpdate();
-    }
-  };
-
   const handleMouseEnter = () => setHover(true);
   const handleMouseLeave = () => setHover(false);
 
   return (
-    <Tooltip
-      title={`${name}: ${value ? numberFormatPercent.format(value) : '-'}`}
-      placement="top"
-      arrow
-      onMouseMove={handleMouseMove}
-      PopperProps={{
-        popperRef,
-        anchorEl: {
-          clientHeight: 0,
-          clientWidth: 0,
-          getBoundingClientRect: () => ({
-            top: positionRef.current.y,
-            left: positionRef.current.x,
-            right: positionRef.current.x,
-            bottom: positionRef.current.y,
-            width: 0,
-            height: 0,
-          }),
-        },
-      }}
-    >
+    <ExtendedTooltip title={tooltip} placement="top" arrow followCursor>
       <path
         d={path}
-        fill={color(value || 0)}
+        fill={hover ? lighten(color, 0.1) : color}
         stroke={theme.palette.background.paper}
         strokeWidth={0.5}
         onMouseEnter={handleMouseEnter}
         onMouseLeave={handleMouseLeave}
-        opacity={hover ? 0.8 : 1}
       />
-    </Tooltip>
+    </ExtendedTooltip>
   );
 }
 
@@ -111,27 +120,35 @@ function WorldMapContent({ values, width, height }: WorldMapContentProps) {
     <svg width={width} height={height}>
       <Mercator
         data={world.features}
+        // Fits the world map to exclude big antarctica landmass in mercator projection
         fitExtent={[
           [
             [0, 0],
-            [width, 1.35 * height],
+            [width, 1.4 * height],
           ],
           world,
         ]}
       >
-        {(mercator) => (
-          <g>
-            {mercator.features.map(({ feature, path }, i) => (
+        {(mercator) =>
+          mercator.features.map(({ feature, path }, i) => {
+            const { name } = feature.properties;
+            const value = values[feature.id];
+            const countryColor =
+              typeof value === 'number'
+                ? color(value)
+                : theme.palette.grey[700];
+            return (
               <Country
-                name={feature.properties.name}
                 key={feature.id}
+                tooltip={`${name}: ${
+                  value ? numberFormatPercent.format(value) : '-'
+                }`}
                 path={path || ''}
-                value={values[feature.id]}
-                color={color}
+                color={countryColor}
               />
-            ))}
-          </g>
-        )}
+            );
+          })
+        }
       </Mercator>
     </svg>
   );
